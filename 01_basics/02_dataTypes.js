f2bc37f9941472291d51c395539d959c912f4d32
Sarfaraj Sarkar <156774338+srfrjsrkr123@users.noreply.github.com>
2024-01-16 09:31:37 +0000

    cleared concepts

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Sarfaraj")
+
+
+let name = "Sarfaraj"
+let age = 28
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+// Null is an Object - Interview Question //
