c0b0696dd1f6174f599fc83013dca7027d345b57
Sarfaraj Sarkar <156774338+srfrjsrkr123@users.noreply.github.com>
2024-01-16 17:10:19 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,43 @@
+//  #Primitive data types
+// 7 types
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt, 
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+/*
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+*/
+
+const bigNumber = 354587951474887n
+// add n to large number , it will be bigInt 
+
+
+
+// Non-Primitive data types
+// types : Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj =
+{
+    name : "sarfaraj" ,
+    age : 28 ,
+}
+console.log(myObj);
+
+
+const myFunction = function()
+{
+    console.log("Hello I am Sarfaraj");
+}
+console.log(typeof myFunction);
+// values under curly braces are called Objects //
+
+
