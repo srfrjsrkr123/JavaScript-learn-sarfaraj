825857a08db75ab3185ae4ea0b740277daa2dfa8
Sarfaraj Sarkar <156774338+srfrjsrkr123@users.noreply.github.com>
2024-01-16 11:18:05 +0000

    edited

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,8 @@
+let score= "123abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumbers);
+console.log(valueInNumbers);
